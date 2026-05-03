0161ce8f99963f0f5b79d7735885e23325f5e8f6
PedroHBecker <pedro.h.beckerfelix@gmail.com>
2026-05-03 15:29:44 -0300

    adicionando Grupos ao contexto

--- ProjetoFinal/LocalNet/LocalNet.API/Data/AppDataContext.cs
+++ ProjetoFinal/LocalNet/LocalNet.API/Data/AppDataContext.cs
@@ -8,7 +8,9 @@ public class AppDataContext : DbContext
 {
     public DbSet<Mensagem> Mensagens { get; set; }
     public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Grupo> Grupos { get; set; }
     public DbSet<UsuarioGrupo> UsuarioGrupos { get; set; }
+    
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
